3488d0890d21cc59e656ffed65d54ba458840a3a
Yaroslav Los <yarroslavlos@gmail.com>
2025-09-23 14:31:41 +0300

    lab2

---
+++ lab2/app.py
@@ -0,0 +1,12 @@
+import time
+from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
+from counterfit_connection import CounterFitConnection
+CounterFitConnection.init('127.0.0.1', 5000)
+
+light_sensor = GroveLightSensor(46)
+
+while True:
+    light = light_sensor.light
+    print('Light level:', light)
+    time.sleep(1)
+
